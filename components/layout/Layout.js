4585807bd1c0e24104bd56666e32037da280d4cb
Ryan Rogers <ryan@ryro.io>
2021-08-27 18:26:18 -0400

    fixing title

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title>{"ryrov2"}</title>
+                <title>{"RyRo.io: For all your Ryan Rogers needs."}</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link
             href="https://fonts.googleapis.com/css2?family=Kameron:wght@400;700&family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,600;1,700;1,800"
